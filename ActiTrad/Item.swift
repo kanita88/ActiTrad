abfaf47baeaef8fbbc6e05c83d4c48586259c4c1
Kanita88 <kanitalim88@gmail.com>
2024-06-03 14:14:06 +0200

    Initial Commit

---
+++ ActiTrad/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ActiTrad
+//
+//  Created by Apprenant 172 on 03/06/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
