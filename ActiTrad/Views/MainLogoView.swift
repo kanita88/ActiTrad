722ae25e660c9b966046c9dfb6e34c7b5b5551f8
Kanita88 <kanitalim88@gmail.com>
2024-06-05 17:18:58 +0200

    commit again

---
+++ ActiTrad/Views/MainLogoView.swift
@@ -0,0 +1,18 @@
+//
+//  MainLogoView.swift
+//  ActiTrad
+//
+//  Created by Apprenant 172 on 05/06/2024.
+//
+
+import SwiftUI
+
+struct MainLogoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainLogoView()
+}
