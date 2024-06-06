dcaf1ca22845b6aeb993cd8501bb33d4d8c9f86e
“diakhite-sio” <diakhite.ibrahima.sio@gmail.com>
2024-06-06 16:28:14 +0200

    Commit Reset branch

---
+++ ActiTrad/Views/EcranFiiltrage.swift
@@ -0,0 +1,175 @@
+//
+//  EcranFiiltrage.swift
+//  ActiTrad
+//
+//  Created by Apprenant 172 on 03/06/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct EcranFiiltrage: View {
+    
+    
+    let arrayCountry = ["France","Espagne","Sénégal","Mali","Inde","Japon"]
+    let arrayCity = ["Paris","Madrid","Dakar","Tombouctou","Tokyo","Bangkok","Rome"]
+    let arrayCategorize = ["Categorize1","Categorize2","Categorize3"]
+    @State var priceBudget:Double = 0
+    @State private var selection = ""
+    
+    var body: some View {
+        
+        ZStack
+        {
+            LinearGradient(gradient: Gradient(colors: [.colorYellowPallette,.colorOrangePallette]), startPoint: .top, endPoint: .bottom)
+            
+           
+            VStack
+            {
+                Text("FIilter")
+                    .font(.system(size: 40))
+                    .padding(.bottom)
+                    .padding(.leading, -150)
+                //Liste Déroulante
+                pickerList(arrayName: arrayCountry, nameList: "Country", selection: $selection)
+                pickerList(arrayName: arrayCity, nameList: "City", selection: $selection)
+                pickerList(arrayName: arrayCategorize, nameList: "Categorize", selection: $selection)
+                
+                
+                //Slider
+                VStack
+                {
+                    Text("Budget \(priceBudget, specifier: "%.0f") $")
+                        
+                    HStack
+                    {
+                        Text("$")
+                        Slider(value: $priceBudget, in :0...500 )
+                            .frame(width: 280)
+                            .accentColor(Color(.colorOrangePallette))
+                        Text("$$$")
+                    }
+                }
+                .padding(.top,40)
+                
+                
+                
+                //Boutton d'actions
+                HStack(spacing : 50)
+                {
+                    //Boutton valider
+                    Button(action:
+                            {/*Changer button par navigation link*/},
+                           label:
+                            
+                            { /*le contenu dans la navigation link*/
+                        ZStack
+                        {
+                            Rectangle()
+                                .foregroundStyle(Color(.colorButton))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .frame(width: 100,height: 50)
+                            Text("Accept")
+                                .foregroundStyle(Color(.white))
+                            
+                        }
+                        })
+                    //Boutton Annuler
+                    Button(action:
+                            {/*Changer button par navigation link*/},
+                           
+                           label:
+                            
+                            {/*le contenu dans la navigation link*/
+                        ZStack
+                        {
+                            Rectangle()
+                                .foregroundStyle(Color(.colorButton))
+                                .clipShape(RoundedRectangle(cornerRadius: 5))
+                                .frame(width: 100,height: 50)
+                            Text("Cancel")
+                                .foregroundStyle(Color(.white))
+                            
+                        }
+                        })
+                }.padding(.top, 50)
+                
+            }
+            
+        }.ignoresSafeArea()
+        
+    }
+}
+
+
+
+
+struct pickerList:View
+{
+    var arrayName:[String] = []
+    var nameList:String
+    @Binding var selection:String
+    
+    var body : some View
+    {
+        VStack(alignment : .leading)
+        {
+            Text(nameList)
+                .font(.system(size: 20))
+                .frame(maxWidth: 250, alignment: .leading)
+            //.frame(width: 50,height: 50)
+        }
+        
+        
+        VStack
+        {
+            
+          
+            ZStack{
+               Rectangle()
+                    .foregroundStyle(Color(.white))
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .frame(width: 250,height: 50)
+                
+                
+                
+                
+                Picker(nameList, selection: $selection)
+                {
+                    
+                    ForEach(arrayName, id: \.self)
+                    {
+                        Text($0)
+                            .foregroundStyle(Color(.black))
+                            
+                    }
+                    
+                }
+                .accentColor(.black)
+                .frame(maxWidth: 220, alignment: .leading)
+                
+                
+                
+                
+                
+            }
+           
+            
+            
+        }
+        
+        
+        //mzlejfmlsegjmozegjfmogz
+        
+    }
+    
+    
+    
+    
+    
+}
+
+#Preview {
+    EcranFiiltrage()
+        .modelContainer(for: Item.self, inMemory: true)
+}
--- ActiTrad/Views/MaScreen.swift
+++ ActiTrad/Views/MaScreen.swift
@@ -40,143 +40,151 @@ struct MaScreen: View {
     
     
     var body: some View {
-        ZStack {
-            //boucle sur locations et afficher le marker dans le map
-            Map(position: $position) {
-                ForEach(locations) { location in
-                    Marker(location.name,coordinate: location.coordinate)
-                }
-            }
-            VStack {
-                HStack{
-                    Image(systemName: "magnifyingglass")
-                    //barre de recherche sur le 1 position
-                    TextField("chercher votre activités", text: $searchTerm, onCommit: {
-                        if search.first != nil {
-                            position = MapCameraPosition.region(
-                                MKCoordinateRegion(
-                                    center: CLLocationCoordinate2D(latitude: 35.682194, longitude: 139.762221),
-                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                                ))
-                        }
-                    })
-                }
-                .padding()
-                .background(Rectangle()
-                    .foregroundColor(.white)
-                    .shadow(radius: 40))
-                .cornerRadius(20)
-                .padding(.horizontal)
-                .padding(.top)
-                .padding(.bottom, 10)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        Button(action: {
-                            position = MapCameraPosition.region(
-                                MKCoordinateRegion(
-                                    center: CLLocationCoordinate2D(latitude: 35.682194, longitude: 139.762221),
-                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                                )
-                            )
-                            
-                            showModal.toggle()
-                            
-                        }) {
-                            Label("Tea ceremony", systemImage: "mug.fill")
-                                .buttonStyle(.plain)
-                                .padding(8)
-                                .background(Color.colorButton)
-                                .foregroundColor(.white)
-                                .clipShape(Capsule())
-                        }
-                        
-                        Button(action: {
-                            
-                            position = MapCameraPosition.region(
-                                MKCoordinateRegion(
-                                    center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                                )
-                            )
-                            
-                            showModal.toggle()
-                            
-                        }) {
-                            Label("Cheese", systemImage: "heart.fill")
-                                .buttonStyle(.plain)
-                                .padding(10)
-                                .background(Color.colorButton)
-                                .foregroundColor(.white)
-                                .clipShape(Capsule())
-                            
-                        }
-                        .sheet(isPresented: $showModal) {
-                            SheetView()
-                                .presentationDetents([.height(300), .large])
-                        }
-                        
-                        Button(action: {
-                            
-                            position = MapCameraPosition.region(
-                                MKCoordinateRegion(
-                                    center: CLLocationCoordinate2D(latitude: 41.918639657910816, longitude: 12.483113871917714),
-                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                                )
-                            )
-                            
-                            showModal.toggle()
-                            
-                        }) {
-                            Label("Masque", systemImage: "theatermasks")
-                                .buttonStyle(.plain)
-                                .padding(10)
-                                .background(Color.colorButton)
-                                .foregroundColor(.white)
-                                .clipShape(Capsule())
-                        }
-                        
-                        Button(action: {
-                            
-                            position = MapCameraPosition.region(
-                                MKCoordinateRegion(
-                                    center: CLLocationCoordinate2D(latitude: 13.761229251291944, longitude: 100.52529107842545),
-                                    span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-                                )
-                            )
-                            
-                            showModal.toggle()
-                            
-                        }) {
-                            Label("Thai danse", systemImage: "flame.fill")
-                                .buttonStyle(.plain)
-                                .padding(10)
-                                .background(Color.colorButton)
-                                .foregroundColor(.white)
-                                .clipShape(Capsule())
-                        }
-                        
-                        Button(action: {
-                            
-                            //Ecran filtre
-                            
-                        }) {
-                            Label("Plus", systemImage: "plus.app.fill")
-                                .buttonStyle(.plain)
-                                .padding(10)
-                                .background(Color.colorButton)
-                                .foregroundColor(.white)
-                                .clipShape(Capsule())
-                        }
-                        
+       NavigationStack
+        {
+            ZStack {
+                //boucle sur locations et afficher le marker dans le map
+                Map(position: $position) {
+                    ForEach(locations) { location in
+                        Marker(location.name,coordinate: location.coordinate)
                     }
                 }
-                .padding(.horizontal)
-                .padding(.bottom)
-                Spacer()
+                VStack {
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                        //barre de recherche sur le 1 position
+                        TextField("chercher votre activités", text: $searchTerm, onCommit: {
+                            if search.first != nil {
+                                position = MapCameraPosition.region(
+                                    MKCoordinateRegion(
+                                        center: CLLocationCoordinate2D(latitude: 35.682194, longitude: 139.762221),
+                                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                    ))
+                            }
+                        })
+                    }
+                    .padding()
+                    .background(Rectangle()
+                        .foregroundColor(.white)
+                        .shadow(radius: 40))
+                    .cornerRadius(20)
+                    .padding(.horizontal)
+                    .padding(.top)
+                    .padding(.bottom, 10)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack {
+                            Button(action: {
+                                position = MapCameraPosition.region(
+                                    MKCoordinateRegion(
+                                        center: CLLocationCoordinate2D(latitude: 35.682194, longitude: 139.762221),
+                                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                    )
+                                )
+                                
+                                showModal.toggle()
+                                
+                            }) {
+                                Label("Tea ceremony", systemImage: "mug.fill")
+                                    .buttonStyle(.plain)
+                                    .padding(8)
+                                    .background(Color.colorButton)
+                                    .foregroundColor(.white)
+                                    .clipShape(Capsule())
+                            }
+                            
+                            Button(action: {
+                                
+                                position = MapCameraPosition.region(
+                                    MKCoordinateRegion(
+                                        center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
+                                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                    )
+                                )
+                                
+                                showModal.toggle()
+                                
+                            }) {
+                                Label("Cheese", systemImage: "heart.fill")
+                                    .buttonStyle(.plain)
+                                    .padding(10)
+                                    .background(Color.colorButton)
+                                    .foregroundColor(.white)
+                                    .clipShape(Capsule())
+                                
+                            }
+                            .sheet(isPresented: $showModal) {
+                               ModalListLieux()
+                                    .presentationDetents([.height(300), .large])
+                            }
+                            
+                            Button(action: {
+                                
+                                position = MapCameraPosition.region(
+                                    MKCoordinateRegion(
+                                        center: CLLocationCoordinate2D(latitude: 41.918639657910816, longitude: 12.483113871917714),
+                                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                    )
+                                )
+                                
+                                showModal.toggle()
+                                
+                            }) {
+                                Label("Masque", systemImage: "theatermasks")
+                                    .buttonStyle(.plain)
+                                    .padding(10)
+                                    .background(Color.colorButton)
+                                    .foregroundColor(.white)
+                                    .clipShape(Capsule())
+                            }
+                            
+                            Button(action: {
+                                
+                                position = MapCameraPosition.region(
+                                    MKCoordinateRegion(
+                                        center: CLLocationCoordinate2D(latitude: 13.761229251291944, longitude: 100.52529107842545),
+                                        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+                                    )
+                                )
+                                
+                                showModal.toggle()
+                                
+                            }) {
+                                Label("Thai danse", systemImage: "flame.fill")
+                                    .buttonStyle(.plain)
+                                    .padding(10)
+                                    .background(Color.colorButton)
+                                    .foregroundColor(.white)
+                                    .clipShape(Capsule())
+                            }
+                            
+                            NavigationLink(destination: EcranFiiltrage())
+                            {
+                                HStack
+                                {
+                                    Text("Plus")
+                                    Image(systemName: "plus.app.fill")
+                                }
+                                .buttonStyle(.plain)
+                                .padding(10)
+                                .background(Color.colorButton)
+                                .foregroundColor(.white)
+                                .clipShape(Capsule())
+                        
+                                
+                                   
+                                       
+                            }
+                            
+                        }
+                    }
+                    .padding(.horizontal)
+                    .padding(.bottom)
+                    Spacer()
+                    
+                }
                 
             }
-            
         }
     }
 }
