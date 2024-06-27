b91d21aeae549c75a59def40e1ff81d4fe066f9f
Kanita <kanitalim88@gmail.com>
2024-06-27 16:18:40 +0200

    update

---
+++ ActiTrad/Models/ActivityClass.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityClass.swift
+//  ActiTrad
+//
+//  Created by Apprenant 172 on 27/06/2024.
+//
+
+import Foundation
---
+++ ActiTrad/Views/AddActivity.swift
@@ -0,0 +1,18 @@
+//
+//  AddActivity.swift
+//  ActiTrad
+//
+//  Created by Apprenant 172 on 26/06/2024.
+//
+
+import SwiftUI
+
+struct AddActivity: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddActivity()
+}
